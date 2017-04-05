68577923f249c44c3ae0d641a8bed192e8ea21c1
Nezar Dimitri <dimitn@mcmaster.ca>
2017-04-05 16:28:49 -0400

    Nav drawer code

---
+++ NavDrawer/app/src/main/java/com/example/nezardimitri/navdrawer/SecondFragment.java
@@ -0,0 +1,25 @@
+package com.example.nezardimitri.navdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nezardimitri on 2017-04-05.
+ */
+
+public class SecondFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+
+    }
+}
